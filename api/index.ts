f5c4f7563574213f9cbc4697d01550e4f6e60203
SayMOSA <moude011@gmail.com>
2026-09-17 23:07:55 +0300

    Delete api directory

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from 'express';
-import { createNestApp } from '../src/bootstrap';
-
-export default async (req: Request, res: Response) => {
-  const app = await createNestApp();
-  app(req, res);
-};
